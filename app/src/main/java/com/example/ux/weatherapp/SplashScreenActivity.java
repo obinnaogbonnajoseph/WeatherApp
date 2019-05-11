69a436da4fa3d609b8b3f54c087761ba9cbcf376
Kene Iloeje <kiloeje@byteworks.com.ng>
2019-05-11 15:45:03 +0100

    Splash Screen

---
+++ app/src/main/java/com/example/ux/weatherapp/SplashScreenActivity.java
@@ -0,0 +1,26 @@
+package com.example.ux.weatherapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private static final int SPLASH_SCREEN_TIME_OUT = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
